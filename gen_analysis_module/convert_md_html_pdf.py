4c4d5560b089ebffbd4dc3873e8d6d8e038bc67d
Jim Delproposto <delpropo@umich.edu>
2024-12-05 09:56:53 -0500

    added prompt reformat as a function in gen_analysis.py

--- gen_analysis_module/convert_md_html_pdf.py
+++ gen_analysis_module/convert_md_html_pdf.py
@@ -5,6 +5,7 @@ import weasyprint
 from gen_analysis_module.config import CSS_CONTENT, PROMPTS_JSON_PATH
 import os
 import json
+from gen_analysis_module.gen_analysis import prompt_reformat
 
 
 def html_class_assignment(prompts_json_path, CSS_CONTENT, full_html_content):
@@ -13,17 +14,16 @@ def html_class_assignment(prompts_json_path, CSS_CONTENT, full_html_content):
         prompts = json.load(file)
 
     # create a key value pair where the key is the prompt and the value is <h2 class="omim_prompt">
-    html_update_dicitonary = {key: f'<h2 class="{key}">' for key in prompts.keys()}
-
+    # html_update_dictionary = {key: f'<h2 class="{key}">' for key in prompts.keys()}
+    # html_update_dictionary = {prompt_reformat(key): f'<h2 class="{prompt_reformat(key)}">' for key in prompts.keys()}
+    html_update_dictionary = {key: f'<h2 class="{key}">' for key in prompts.keys()}
+    # html_update_dictionary = {prompt_reformat(key): f'<h2 class="{prompt_reformat(key)}">' for key in prompts.keys()}
     # open the full_html_content and replace all lines that have <h2> {key} </h2> with  <h2 class="{key}"> {key} </h2>
-    for key, value in html_update_dicitonary.items():
-        full_html_content = full_html_content.replace(f'<h2>*{key}*</h2>', value)
+    for key, value in html_update_dictionary.items():
+        full_html_content = full_html_content.replace(f'<h2>*{prompt_reformat(key)}*</h2>', value)
+
 
     return full_html_content
-
-
-
-
 
 
 def markdown_to_html(markdown_file, CSS_CONTENT, prompts_json_path):
@@ -63,11 +63,6 @@ def markdown_to_html(markdown_file, CSS_CONTENT, prompts_json_path):
 
     return html_class_assignment(prompts_json_path, CSS_CONTENT, full_html_content)
 
-
-
-
-
-
 def save_html(full_html_content, output_file_html):
     with open(output_file_html, 'w') as html_file:
         html_file.write(full_html_content)
@@ -77,9 +72,9 @@ def save_pdf(full_html_content, output_file_pdf):
     weasyprint.HTML(string=full_html_content).write_pdf(output_file_pdf)
 
 
-def complete_html_pdf(markdown_file, CSS_CONTENT):
+def complete_html_pdf(markdown_file, CSS_CONTENT, prompts_json_path):
     """
-    Combines all functions to convert markdown to html and pdf and save the files.
+    Combines all functions to convert markdown to html and pdf and saves the files.
     Args:
         markdown_file (str): The full path to the Markdown file to be converted.
         CSS_CONTENT (str): A string containing CSS styles to be applied to the HTML content.  This applies the color scheme to the code blocks.
@@ -99,7 +94,7 @@ def complete_html_pdf(markdown_file, CSS_CONTENT):
             output_file_pdf = base_filename + '.pdf'
 
         # Convert Markdown to HTML with Syntax Highlighting
-        full_html_content = markdown_to_html(markdown_file, CSS_CONTENT)
+        full_html_content = markdown_to_html(markdown_file, CSS_CONTENT, prompts_json_path)
 
         save_html(full_html_content, output_file_html)
         save_pdf(full_html_content, output_file_pdf)
@@ -139,11 +134,8 @@ if __name__ == "__main__":
 
 
 
-    if complete_html_pdf(markdown_file, CSS_CONTENT):
+    if complete_html_pdf(markdown_file, CSS_CONTENT, PROMPTS_JSON_PATH):
         print("HTML and PDF files generated successfully with syntax highlighting.")
     else:
         print("An error occurred during file conversion.")
 
-    # prompts = prompts_color_assignment(PROMPTS_JSON_PATH, CSS_CONTENT)
-
-    # print(prompts)
